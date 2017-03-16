d85c051b5f968be93550d2a335e0c51877130d94
Cesar Sevilla <geo.sevilla@gmail.com>
2017-03-15 21:27:01 -0500

    all commit files

---
+++ helloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloWorld
+//
+//  Created by macbook air on 8/23/15.
+//  Copyright (c) 2015 Berserk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloWorld
+//
+//  Created by macbook air on 8/23/15.
+//  Copyright (c) 2015 Berserk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
